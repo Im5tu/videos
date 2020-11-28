cbf02c845d2cd7e7637fe9acc5340b2ad18827b8
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2020-11-28 18:18:45 +0000

    Formatting

--- Orleans/2 - Microsoft Orleans - Persisting State/HelloOrleans/HelloWorldGrain.cs
+++ Orleans/2 - Microsoft Orleans - Persisting State/HelloOrleans/HelloWorldGrain.cs
@@ -1,7 +1,4 @@
 ﻿using Orleans;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace HelloOrleans
--- Orleans/2 - Microsoft Orleans - Persisting State/HelloOrleans/Program.cs
+++ Orleans/2 - Microsoft Orleans - Persisting State/HelloOrleans/Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Orleans.Hosting;
 
 namespace HelloOrleans
@@ -23,9 +17,9 @@ namespace HelloOrleans
                 {
                     webBuilder.UseStartup<Startup>();
                 })
-            .UseOrleans(builder =>
-            {
-                builder.UseLocalhostClustering();
-            });
+                .UseOrleans(builder =>
+                {
+                    builder.UseLocalhostClustering();
+                });
     }
 }
