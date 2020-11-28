7f876a9e573e63f15570f21f1340e8e12367e505
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2020-11-28 16:37:37 +0000

    Start of ep2

---
+++ Orleans/2 - Microsoft Orleans - Persisting State/HelloOrleans/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloOrleans
+{
+    public class HelloWorldController : Controller
+    {
+        private readonly IClusterClient clusterClient;
+
+        public HelloWorldController(IClusterClient clusterClient)
+        {
+            this.clusterClient = clusterClient;
+        }
+
+        [HttpGet("/hello/{name}")]
+        public async Task<IActionResult> Hello(string name)
+        {
+            var result = await clusterClient.GetGrain<IHelloWorldGrain>("Stu").SayHelloToAsync(name);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Orleans/2 - Microsoft Orleans - Persisting State/HelloOrleans/HelloWorldGrain.cs
@@ -0,0 +1,23 @@
+﻿using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloOrleans
+{
+    public interface IHelloWorldGrain : IGrainWithStringKey
+    {
+        Task<string> SayHelloToAsync(string name);
+    }
+
+    public class HelloWorldGrain : Grain, IHelloWorldGrain
+    {
+        private int _invocationCount = 0;
+
+        public Task<string> SayHelloToAsync(string name)
+        {
+            return Task.FromResult($"Hello {name} from {this.GetPrimaryKeyString()} - I've said hello {_invocationCount++} times.");
+        }
+    }
+}
