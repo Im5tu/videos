dcfe11fed33912690eb29b90d287531d13c9baa4
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2020-12-06 18:37:11 +0000

    Code for vid3

--- Orleans/3 - Microsoft Orleans - Clustering/HelloOrleans/Program.cs
+++ Orleans/3 - Microsoft Orleans - Clustering/HelloOrleans/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Orleans.Configuration;
 using Orleans.Hosting;
+using System.Net;
 
 namespace HelloOrleans
 {
@@ -19,11 +21,27 @@ namespace HelloOrleans
                 })
                 .UseOrleans(builder =>
                 {
-                    builder.UseLocalhostClustering();
+                    builder.UseDynamoDBClustering(options =>
+                    {
+                        options.Service = "http://localhost:4566";
+                    });
                     builder.AddDynamoDBGrainStorageAsDefault(options =>
                     {
                         options.Service = "http://localhost:4566";
                         options.UseJson = true;
+                    });
+
+                    builder.Configure<EndpointOptions>(options =>
+                    {
+                        options.AdvertisedIPAddress = IPAddress.Loopback;
+                        options.GatewayListeningEndpoint = new IPEndPoint(IPAddress.Any, EndpointOptions.DEFAULT_GATEWAY_PORT);
+                        options.SiloListeningEndpoint = new IPEndPoint(IPAddress.Any, EndpointOptions.DEFAULT_SILO_PORT);
+                    });
+
+                    builder.Configure<ClusterOptions>(options =>
+                    {
+                        options.ClusterId = "my-first-cluster";
+                        options.ServiceId = "my-first-service";
                     });
                 });
     }
