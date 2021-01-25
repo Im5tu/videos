3232e8aac716d1c7837cbcfaf51e16bca3e80fea
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2021-01-25 20:51:39 +0000

    Pre-vid

--- Orleans/6 - Microsoft Orleans - Stateless Workers/HelloOrleans/HelloWorldGrain.cs
+++ Orleans/6 - Microsoft Orleans - Stateless Workers/HelloOrleans/HelloWorldGrain.cs
@@ -1,6 +1,4 @@
 using Orleans;
-using Orleans.Runtime;
-using System;
 using System.Threading.Tasks;
 
 namespace HelloOrleans
@@ -10,36 +8,13 @@ namespace HelloOrleans
         Task<string> SayHelloToAsync(string name);
     }
 
-    public class HelloWorldGrain : Grain<HelloState>, IHelloWorldGrain, IRemindable
+    public class HelloWorldGrain : Grain<HelloState>, IHelloWorldGrain
     {
-        // private IDisposable _timer;
-
         public async Task<string> SayHelloToAsync(string name)
         {
             var count = State.InvocationCount++;
             await WriteStateAsync();
             return $"Hello {name} from {this.GetPrimaryKeyString()} - I've said hello {count} times.";
-        }
-
-        public override async Task OnActivateAsync()
-        {
-            //_timer = RegisterTimer(state =>
-            //{
-            //    Console.WriteLine("Hello World");
-            //    return Task.CompletedTask;
-            //}, null, TimeSpan.FromSeconds(5), TimeSpan.FromMinutes(1));
-
-            await RegisterOrUpdateReminder("GrainReminder", TimeSpan.FromSeconds(5), TimeSpan.FromMinutes(1));
-
-            await base.OnActivateAsync();
-        }
-
-        public Task ReceiveReminder(string reminderName, TickStatus status)
-        {
-            if ("GrainReminder".Equals(reminderName))
-                Console.WriteLine("Hello Reminder");
-
-            return Task.CompletedTask;
         }
     }
 
